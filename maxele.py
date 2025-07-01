10c21de6261bddb901eb40303326491ac5199146
ADUPULA-ARUN <a.tarunyadav3@gmail.com>
2025-07-01 10:51:22 +0530

    Update maxele.py

--- maxele.py
+++ maxele.py
@@ -1,7 +1,9 @@
-a=[1,2,4,6,7,5]
-max =a[0]
-min=a[2]
-for i in range(len(a)):
-    if(max < a[i]):
-        max = a[i]
-print(max)
+a = [1, 2, 6, 7, 3, 4]
+first = second = float('-inf')
+for num in a:
+    if num > first:
+        second = first
+        first = num
+    elif num > second and num != first:
+        second = num
+print(second)
